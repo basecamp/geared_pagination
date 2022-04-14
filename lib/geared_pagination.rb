33917af60e725f716581d6139f6fbe50fa3d559b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-14 19:39:30 +0800

    Delete lib directory

--- lib/geared_pagination.rb
+++
@@ -1,2 +0,0 @@
-require 'geared_pagination/recordset'
-require 'geared_pagination/engine' if defined?(Rails)
--- lib/geared_pagination/portions.rb
+++
@@ -1,2 +0,0 @@
-require 'geared_pagination/portions/portion_at_cursor'
-require 'geared_pagination/portions/portion_at_offset'
