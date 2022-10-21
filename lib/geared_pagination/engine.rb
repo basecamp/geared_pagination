eadf4b6e73dec8ae18df337563d2aa2834ce9483
Roger Bagué <rbague@users.noreply.github.com>
2022-10-21 09:49:32 +0000

    enable usage in API-only applications

--- lib/geared_pagination/engine.rb
+++ lib/geared_pagination/engine.rb
@@ -5,6 +5,7 @@ class GearedPagination::Engine < ::Rails::Engine
   initializer :geared_pagination do |app|
     ActiveSupport.on_load :action_controller do
       ActionController::Base.send :include, GearedPagination::Controller
+      ActionController::API.send :include, GearedPagination::Controller
     end
   end
 end
