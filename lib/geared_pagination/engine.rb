84504c73f41f2561207cd60d0e534654f99ee0cb
Marian Posaceanu <contact@marianposaceanu.com>
2023-04-18 23:55:05 +0300

    Add option to configure the cursor/param name

--- lib/geared_pagination/engine.rb
+++ lib/geared_pagination/engine.rb
@@ -1,10 +1,28 @@
+require 'singleton'
 require 'rails/engine'
 require 'geared_pagination/controller'
 
-class GearedPagination::Engine < ::Rails::Engine
-  initializer :geared_pagination do |app|
-    ActiveSupport.on_load :action_controller do
-      ActionController::Base.send :include, GearedPagination::Controller
+module GearedPagination
+  class << self
+    def configure
+      yield config
+    end
+
+    def config
+      @_config ||= Configuration.new
+    end
+  end
+
+  class Configuration
+    include ActiveSupport::Configurable
+    config_accessor :cursor_name, default: :page
+  end
+
+  class Engine < ::Rails::Engine
+    initializer :geared_pagination do |app|
+      ActiveSupport.on_load :action_controller do
+        ActionController::Base.send :include, GearedPagination::Controller
+      end
     end
   end
 end
