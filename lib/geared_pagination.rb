00a356e98e347a0ea66daabfee5b20a3a56e0384
Jacopo <beschi.jacopo@gmail.com>
2023-10-19 12:18:21 +0200

    Handle deprecate API changes
    DEPRECATION WARNING: Module.deprecate without a deprecator is deprecated

--- lib/geared_pagination.rb
+++ lib/geared_pagination.rb
@@ -1,2 +1,14 @@
+require 'active_support/deprecation'
+
+module GearedPagination
+  def self.rails_7_1?
+    Rails.version >= "7.1.0" rescue false
+  end
+
+  def self.deprecator
+    @deprecator ||= ActiveSupport::Deprecation.new("1.1.2", "GearedPagination")
+  end
+end
+
 require 'geared_pagination/recordset'
 require 'geared_pagination/engine' if defined?(Rails)
